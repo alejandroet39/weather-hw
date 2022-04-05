39471f0292538ca9ad90e5d00d72a6bb1c6b0322
alejandroet39 <DRUMSINDEEP@GMAIL.COM>
2022-04-05 22:30:53 +0200

    changes

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ fetch("")
     }
   })
   .then((data) => console.log(data));
+
+function fetchWeather(lat, lon) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=a8aa38cdd3dd713a7207c383fb08def8`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data.daily);
+    });
+}
